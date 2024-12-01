5488b3928021c37501469354de87a2fea0ffa18a
Karimnaggar4040 <150073117+Karimnaggar4040@users.noreply.github.com>
2024-12-02 00:49:40 +0200

    Added users for the trial and testings and added them in an ArrayList in this class.

--- src/java/duckHub/MainDuck.java
+++ src/java/duckHub/MainDuck.java
@@ -1,4 +1,47 @@
 package duckHub;
 
-public class MainDuck {
+import duckHub.backend.*;
+import duckHub.backend.database.Load;
+import duckHub.backend.database.Save;
+import duckHub.frontend.feed.MainScene;
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class MainDuck extends Application {
+    private ArrayList<User> users = new ArrayList<>();
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        User zoz = new User("zoz@zozTheKing.com","ZKING","3ab7amid3absalam", LocalDate.now());
+        User naggar = new User("naggar@naggarElTop.com","NAGGAR","3ab7amid3absalam", LocalDate.now());
+        User roupha = new User("roupha@elRegoula.com","ROUPHA","3ab7amid3absalam", LocalDate.now());
+        User walaa = new User("walaa@01101011 01100101 01110011 01110011 01101111.com","WALAA@ElFoka3a","666666666666", LocalDate.now());
+        users.add(zoz);
+        users.add(naggar);
+        users.add(roupha);
+        users.add(walaa);
+
+        Save save = new Save();
+        save.saveToFile(users);
+
+        Load load = new Load();
+        load.loadFromFile();
+        users = load.getUsers();
+        System.out.println(users);
+        for (User user : users) {
+            System.out.println(user.getUserId());
+            for (Post post : user.getPosts()) {
+                System.out.println(post.getAuthorId());
+                System.out.println(post.getContentImage().getUrl());
+            }
+        }
+    }
 }
