f1ea86778884928c85927fa231cd30d1663e6b06
Karimnaggar4040 <150073117+Karimnaggar4040@users.noreply.github.com>
2024-12-05 01:31:01 +0200

    add a rectangle image button in the ButtonCustomizer.

--- src/java/duckHub/frontend/feed/ButtonCustomizer.java
+++ src/java/duckHub/frontend/feed/ButtonCustomizer.java
@@ -5,6 +5,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 public class ButtonCustomizer {
     public void roundedButtonImage(ImageView newContentImgView, Button newContentButton) {
@@ -17,6 +18,21 @@ public class ButtonCustomizer {
         StackPane stackPane = new StackPane();
         stackPane.getChildren().addAll(newContentImgView);
 
+        newContentButton.setGraphic(newContentImgView);
+        newContentButton.setStyle("-fx-background-color: transparent;");
+    }
+
+    public void rectangleButtonImage(ImageView newContentImgView, Button newContentButton) {
+        double width = 15;
+        double height = 15;
+        newContentImgView.setFitWidth(width);
+        newContentImgView.setFitHeight(height);
+
+        Rectangle newContentButtonShape = new Rectangle(width, height, width/2, height/2);
+        newContentImgView.setClip(newContentButtonShape);
+
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().addAll(newContentImgView);
         newContentButton.setGraphic(newContentImgView);
         newContentButton.setStyle("-fx-background-color: transparent;");
     }
