312466b52d42ff4f7b81280f0bc4a39432bf9f09
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2024-12-06 05:40:09 +0200

    added edit buttons

--- src/java/duckHub/frontend/profile/Profile.java
+++ src/java/duckHub/frontend/profile/Profile.java
@@ -3,14 +3,19 @@ package duckHub.frontend.profile;
 import duckHub.MainDuck;
 import duckHub.backend.User;
 import duckHub.frontend.ContentConvertor;
+import duckHub.frontend.SizeConstants;
+import duckHub.frontend.feed.ButtonCustomizer;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
@@ -30,8 +35,13 @@ public class Profile {
     private HBox friendsHBoxLayout;
     private ScrollPane allPostsScrollPane;
     private VBox postsVBoxLayout;
+    private HBox editButtonsHBoxLayout;
 
     private TextArea bioContent;
+
+    Button changeCoverButton;
+    Button changeProfileButton;
+    Button changeBioButton;
 
     private User user;
 
@@ -42,6 +52,8 @@ public class Profile {
         profileRootVBox = new VBox(20);
         // bio layout
         bioLayout = new HBox(20);
+        // edit buttons layout
+        editButtonsHBoxLayout = new HBox(20);
         // friends layout
         allFriendsScrollPane = new ScrollPane();
         friendsHBoxLayout = new HBox();
@@ -81,8 +93,8 @@ public class Profile {
         double radius = 70;
         Image profilePhoto = user.getUserProfileImage();
         ImageView profilePhotoView = new ImageView(profilePhoto);
-        profilePhotoView.setFitHeight(radius * 2);
-        profilePhotoView.setFitWidth(radius * 2);
+        profilePhotoView.setFitHeight(radius*2);
+        profilePhotoView.setFitWidth(radius*2);
         Circle newContentButtonShape = new Circle(radius, radius, radius);
         profilePhotoView.setClip(newContentButtonShape);
 
@@ -91,13 +103,22 @@ public class Profile {
         bioContent.setEditable(Boolean.FALSE);
         bioContent.setWrapText(true);
         bioContent.setPrefHeight(2 * radius);
-        bioContent.prefWidthProperty().bind(profileScene.widthProperty().subtract(2 * radius + 50));
+        bioContent.prefWidthProperty().bind(profileScene.widthProperty().subtract(2 * radius + 70));
 //        bioContent.setText(user.get);
 
         bioLayout.getChildren().addAll(profilePhotoView, bioContent);
         bioLayout.setPadding(new Insets(0, 10, 0, 10));
 
         profileRootVBox.getChildren().add(bioLayout);
+    }
+
+    private void showProfileEdits(){
+        changeCoverButton = new Button("Change Cover Photo");
+        changeProfileButton = new Button("Change Profile Photo");
+        changeBioButton = new Button("Change Bio Content");
+        editButtonsHBoxLayout.getChildren().addAll(changeCoverButton, changeProfileButton, changeBioButton);
+        editButtonsHBoxLayout.setAlignment(Pos.CENTER);
+        profileRootVBox.getChildren().add(editButtonsHBoxLayout);
     }
 
     private void showFriends() {
@@ -119,6 +140,7 @@ public class Profile {
         setScene();
         showCoverPhoto(user);
         showBioContent(user);
+        showProfileEdits();
         showFriends();
         showPosts(user);
         return profileScene;
