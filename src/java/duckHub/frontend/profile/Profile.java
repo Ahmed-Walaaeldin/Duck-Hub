c71f7c370b4b7e89ecd6ea1f2d8ab1a2d2111a66
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2024-12-06 04:44:21 +0200

    added features to profile scene : cover photo, profile photo, bio, friends, posts

--- src/java/duckHub/frontend/profile/Profile.java
+++ src/java/duckHub/frontend/profile/Profile.java
@@ -1,50 +1,126 @@
 package duckHub.frontend.profile;
 
-import javafx.application.Application;
+import duckHub.MainDuck;
+import duckHub.backend.User;
+import duckHub.frontend.ContentConvertor;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
-import java.io.File;
 
-public class Profile extends Application {
-    public static void main(String[] args) {
-        launch(args);
+public class Profile {
+    // to test
+    private Stage stage;
+
+    // Displayed Scene
+    private Scene profileScene;
+
+    // layouts
+    private ScrollPane profileRootScrollPane;
+    private VBox profileRootVBox;
+    private HBox bioLayout;
+    private ScrollPane allFriendsScrollPane;
+    private HBox friendsHBoxLayout;
+    private ScrollPane allPostsScrollPane;
+    private VBox postsVBoxLayout;
+
+    private TextArea bioContent;
+
+    private User user;
+
+
+    private void layoutInitializer() {
+        // root layout
+        profileRootScrollPane = new ScrollPane();
+        profileRootVBox = new VBox(20);
+        // bio layout
+        bioLayout = new HBox(20);
+        // friends layout
+        allFriendsScrollPane = new ScrollPane();
+        friendsHBoxLayout = new HBox();
+        // posts layout
+        allPostsScrollPane = new ScrollPane();
+        postsVBoxLayout = new VBox();
     }
 
-    @Override
-    public void start(Stage window) {
-        //cover photo
-        Image coverPhoto = new Image(new File("path").toURI().toString());
+    private void layoutOrganizer() {
+        profileRootScrollPane.setContent(profileRootVBox);
+
+        allPostsScrollPane.setFitToWidth(true);
+        allPostsScrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        allPostsScrollPane.setContent(postsVBoxLayout);
+
+        allFriendsScrollPane.setContent(friendsHBoxLayout);
+    }
+
+    private void setScene() {
+        profileScene = new Scene(profileRootScrollPane, 600, 500);
+        stage = new Stage();
+        stage.setScene(profileScene);
+        profileRootScrollPane.setFitToWidth(true);
+    }
+
+    private void showCoverPhoto(User user) {
+        Image coverPhoto = user.getUserProfileImage();
         ImageView coverPhotoView = new ImageView(coverPhoto);
+        coverPhotoView.fitWidthProperty().bind(profileScene.widthProperty());
+        coverPhotoView.setFitHeight(70);
 
-        //profile photo
-        Image profilePhoto = new Image(new File("path").toURI().toString());
+        profileRootVBox.getChildren().add(coverPhotoView);
+    }
+
+    private void showBioContent(User user) {
+        // profile photo
+        double radius = 70;
+        Image profilePhoto = user.getUserProfileImage();
         ImageView profilePhotoView = new ImageView(profilePhoto);
+        profilePhotoView.setFitHeight(radius * 2);
+        profilePhotoView.setFitWidth(radius * 2);
+        Circle newContentButtonShape = new Circle(radius, radius, radius);
+        profilePhotoView.setClip(newContentButtonShape);
 
-        //Bio
-        Label nameLabel = new Label();
-        nameLabel.setText("Name Name");
-        Label bioLabel = new Label();
-        bioLabel.setText("Bio");
-        VBox bioLayout = new VBox(bioLabel);
+        // bio content
+        bioContent = new TextArea();
+        bioContent.setEditable(Boolean.FALSE);
+        bioContent.setWrapText(true);
+        bioContent.setPrefHeight(2 * radius);
+        bioContent.prefWidthProperty().bind(profileScene.widthProperty().subtract(2 * radius + 50));
+//        bioContent.setText(user.get);
 
-        //Friends Layout
-        HBox friendsLayout = new HBox();
-        friendsLayout.getChildren().addAll();
-        
-        //Posts Layout
-        VBox postsLayout = new VBox();
-        postsLayout.getChildren().addAll();
+        bioLayout.getChildren().addAll(profilePhotoView, bioContent);
+        bioLayout.setPadding(new Insets(0, 10, 0, 10));
 
-        VBox profileRootLayout = new VBox(coverPhotoView, profilePhotoView, bioLayout, friendsLayout, postsLayout);
-        Scene profileScene = new Scene(profileRootLayout, 600, 700);
-        window.setScene(profileScene);
-        window.setTitle("Profile");
-        window.show();
+        profileRootVBox.getChildren().add(bioLayout);
+    }
+
+    private void showFriends() {
+        // yala ya zoz
+        profileRootVBox.getChildren().add(allFriendsScrollPane);
+    }
+
+    private void showPosts(User user) {
+        ContentConvertor contentConvertor = new ContentConvertor();
+        contentConvertor.convertPostsToNodes(user,allPostsScrollPane , postsVBoxLayout);
+        allPostsScrollPane.setPadding(new Insets(0, 10, 0, 10));
+        profileRootVBox.getChildren().add(allPostsScrollPane);
+    }
+
+    public Scene displayScene(MainDuck mainDuck, User user) {
+        this.user = user;
+        layoutInitializer();
+        layoutOrganizer();
+        setScene();
+        showCoverPhoto(user);
+        showBioContent(user);
+        showFriends();
+        showPosts(user);
+        return profileScene;
     }
 }
