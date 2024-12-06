4c63e945ed1d7d3d9a04c79be1996202ca84e475
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2024-12-06 04:46:02 +0200

    added profile scene to MainDuck

--- src/java/duckHub/MainDuck.java
+++ src/java/duckHub/MainDuck.java
@@ -1,10 +1,17 @@
 package duckHub;
 
+import duckHub.backend.BackendDuck;
+import duckHub.backend.User;
 import duckHub.frontend.LoginPage;
 import duckHub.frontend.SignupPage;
+import duckHub.frontend.profile.Profile;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.time.LocalDate;
 
 public class MainDuck extends Application {
     private Stage primaryStage;
@@ -12,29 +19,39 @@ public class MainDuck extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
 
         primaryStage.setTitle("DuckHub");
         showLoginPage(); // Start with Login Page
-
         primaryStage.show();
     }
+
     public void showLoginPage() {
         System.out.println("Switching to Login Page");
         LoginPage loginPage = new LoginPage();
         Scene loginScene = loginPage.getScene(this);
         primaryStage.setScene(loginScene);
     }
+
     public void showSignupPage() {
         System.out.println("Switching to Signup Page");
         SignupPage signupPage = new SignupPage();
         Scene signupScene = signupPage.getScene(this);
         primaryStage.setScene(signupScene);
     }
-    public void showNewsfeed(){
+
+    public void showNewsfeed() {
         System.out.println("Switching to Newsfeed Page");
         // yalla ya nigo
+    }
+
+    public void showProfilePage() {
+        System.out.println("Switching to Profile Page");
+        Profile profile = new Profile();
+        Scene profileScene = profile.displayScene(this, user);
+        primaryStage.setScene(profileScene);
     }
 }
