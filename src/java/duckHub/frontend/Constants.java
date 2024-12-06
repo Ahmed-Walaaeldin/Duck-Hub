aaeff6cd0aaca7f973a1b85021981c08a6eb9878
Karimnaggar4040 <150073117+Karimnaggar4040@users.noreply.github.com>
2024-12-06 16:20:17 +0200

    Added a constant file name for the user data and made the classes implement the constants interface. Also made the stories and posts to appear are only the friends posts and stories.

--- src/java/duckHub/frontend/Constants.java
+++ src/java/duckHub/frontend/Constants.java
@@ -1,8 +1,9 @@
 package duckHub.frontend;
 
-public interface SizeConstants {
+public interface Constants {
     double SCENE_WIDTH = 600;
     double SCENE_HEIGHT = 500;
     double PROFILE_IMAGE_RADIUS = 25.0;
     double ROUNDED_BUTTON_RADIUS = 20.0;
+    String JSON_FILE_NAME = "user_data.json";
 }
